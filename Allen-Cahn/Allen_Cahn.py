8da2732582fb42055247628bed9c3f60063d96a1
Yifan Chen <yifanc96@gmail.com>
2021-10-17 22:06:33 -0700

    Update Allen-Cahn

--- Allen-Cahn/Allen_Cahn.py
+++ Allen-Cahn/Allen_Cahn.py
@@ -9,6 +9,8 @@ config.update("jax_enable_x64", True)
 import numpy as onp
 from numpy import random 
 
+import scipy.io
+
 import argparse
 import logging
 import datetime
@@ -17,13 +19,13 @@ import os
 
 
 def get_parser():
-    parser = argparse.ArgumentParser(description='Burgers equation GP solver')
-    parser.add_argument("--nu", type=float, default = 0.01/onp.pi)
+    parser = argparse.ArgumentParser(description='Allen Cahn equation GP solver')
+    parser.add_argument("--nu", type=float, default = 1e-4)
     parser.add_argument("--kernel", type=str, default="gaussian", choices=["gaussian","inv_quadratics"])
-    parser.add_argument("--sigma", type = float, default = 0.01)
+    parser.add_argument("--sigma", type = float, default = 0.02)
     parser.add_argument("--dt", type = float, default = 0.04)
     parser.add_argument("--T", type = float, default = 1.0)
-    parser.add_argument("--N_domain", type = int, default = 400)
+    parser.add_argument("--N_domain", type = int, default = 512)
     parser.add_argument("--nugget", type = float, default = 1e-13)
     parser.add_argument("--GNsteps", type = int, default = 2)
     
@@ -35,19 +37,16 @@ def get_parser():
 def sample_points(num_pts, dt, T, option = 'grid'):
     Nt = int(T/dt)+1
     X_domain = onp.zeros((Nt,num_pts,2))
-    X_boundary = onp.zeros((Nt,2,2))
     if option == 'grid':
         for i in range(Nt):
             X_domain[i,:,0] = i*dt
             X_domain[i,:,1] = onp.linspace(-1.0,1.0, num_pts)
-            X_boundary[i,:,0] = i*dt
-            X_boundary[i,:,1] = [-1.0,1.0]
-    return X_domain, X_boundary
+    return X_domain
 
 
-def assembly_Theta(X_domain, X_boundary, sigma):
+def assembly_Theta(X_domain, sigma):
     N_domain = onp.shape(X_domain)[0]
-    N_boundary = onp.shape(X_boundary)[0]
+    N_boundary = 0
     Theta = onp.zeros((3*N_domain+N_boundary, 3*N_domain+N_boundary))
     
     # auxiliary vector for construncting Theta
@@ -56,7 +55,7 @@ def assembly_Theta(X_domain, X_boundary, sigma):
     XdXd_T = onp.transpose(XdXd) 
     
     XdXb1 = onp.transpose(onp.tile(X_domain,(N_domain+N_boundary,1)))
-    X_all = onp.concatenate([X_domain,X_boundary])
+    X_all = X_domain
     XdXb2 = onp.tile(X_all,(N_domain,1))
     
     XdbXdb = onp.tile(X_all,(N_domain+N_boundary,1))
@@ -85,40 +84,37 @@ def assembly_Theta(X_domain, X_boundary, sigma):
     
     return Theta
 
-def J_loss(v, rhs_f, bdy_g,L,dt):
+def J_loss(v, rhs_f, L,dt):
     N_domain = onp.shape(rhs_f)[0]
     
-    vec_u = v[:N_domain]
-    vec_u_x = v[N_domain:]
-    vec_u_xx = (2/dt*vec_u+vec_u*vec_u_x-rhs_f)/nu
+    vec_u = jnp.append(v[:N_domain-1],v[0])
+    vec_u_x = jnp.append(v[N_domain-1:],v[N_domain-1])
+    vec_u_xx = (2/dt*vec_u+5*vec_u**3-5*vec_u-rhs_f)/nu
     vv = jnp.append(vec_u_xx,vec_u_x)
     vv = jnp.append(vv,vec_u)
-    vv = jnp.append(vv,bdy_g)
     
     temp = jnp.linalg.solve(L,vv)
     return jnp.dot(temp, temp)
 
 grad_J = grad(J_loss)
 
-def GN_J(v, rhs_f, bdy_g,L, dt, v_old):
+def GN_J(v, rhs_f, L, dt, v_old):
     N_domain = onp.shape(rhs_f)[0]
     
-    vec_u_old = v_old[:N_domain]
-    vec_u_x_old = v_old[N_domain:]
+    vec_u_old = jnp.append(v_old[:N_domain-1],v_old[0])
     
-    vec_u = v[:N_domain]
-    vec_u_x = v[N_domain:]
-    vec_u_xx = (2/dt*vec_u+vec_u_old*vec_u_x+vec_u*vec_u_x_old-rhs_f)/nu
+    vec_u = jnp.append(v[:N_domain-1],v[0])
+    vec_u_x = jnp.append(v[N_domain-1:],v[N_domain-1])
+    vec_u_xx = (2/dt*vec_u+15*vec_u_old**2*vec_u-5*vec_u-rhs_f)/nu
     vv = jnp.append(vec_u_xx,vec_u_x)
     vv = jnp.append(vv,vec_u)
-    vv = jnp.append(vv,bdy_g)
     
     temp = jnp.linalg.solve(L,vv)
     return jnp.dot(temp, temp)
 
 Hessian_GN=hessian(GN_J)
 
-def time_steping_solve(X_domain, X_boundary, dt,T,num_pts, step_size = 1, nugget = 1e-10, sigma=0.2, GN_iteration = 4):
+def time_steping_solve(X_domain, dt,T,num_pts, step_size = 1, nugget = 1e-10, sigma=0.2, GN_iteration = 4):
     Nt = int(T/dt)+1
     sol_u = onp.zeros((Nt,num_pts))
     sol_u_x = onp.zeros((Nt,num_pts))
@@ -128,16 +124,15 @@ def time_steping_solve(X_domain, X_boundary, dt,T,num_pts, step_size = 1, nugget
     sol_u_x[0,:]=vmap(u_x)(X_domain[0,:,0],X_domain[0,:,1])
     sol_u_xx[0,:]=vmap(u_xx)(X_domain[0,:,0],X_domain[0,:,1])
     
-    Theta = assembly_Theta(X_domain[0,:,1], X_boundary[0,:,1], sigma)
+    Theta = assembly_Theta(X_domain[0,:,1], sigma)
     N_domain = num_pts
-    N_boundary = 2
     
     # adaptive nugget term
     trace1 = jnp.trace(Theta[:N_domain, :N_domain])
     trace2 = jnp.trace(Theta[N_domain:2*N_domain, N_domain:2*N_domain])
     trace3 = jnp.trace(Theta[2*N_domain:, 2*N_domain:])
     ratio = [trace1/trace3, trace2/trace3]
-    temp=jnp.concatenate((ratio[0]*jnp.ones((1,N_domain)),ratio[1]*jnp.ones((1,N_domain)),jnp.ones((1,N_domain+N_boundary))), axis=1)
+    temp=jnp.concatenate((ratio[0]*jnp.ones((1,N_domain)),ratio[1]*jnp.ones((1,N_domain)),jnp.ones((1,N_domain))), axis=1)
     Theta = Theta + nugget*jnp.diag(temp[0])
     L = jnp.linalg.cholesky(Theta)
     
@@ -145,18 +140,18 @@ def time_steping_solve(X_domain, X_boundary, dt,T,num_pts, step_size = 1, nugget
     for iter_i in range(Nt-1):
         # solve at t = (iter_i+1)*dt
         # get rhs_f and bdy_g
-        rhs_f = 2/dt*sol_u[iter_i,:]-sol_u[iter_i,:]*sol_u_x[iter_i,:]+nu*sol_u_xx[iter_i,:]
-        bdy_g = jnp.array([0,0])
-        sol = jnp.append(sol_u[iter_i,:],sol_u_x[iter_i,:]) # initialization
+        rhs_f = 2/dt*sol_u[iter_i,:]+nu*sol_u_xx[iter_i,:]-5*sol_u[iter_i,:]**3+5*sol_u[iter_i,:]
+        sol = jnp.append(sol_u[iter_i,0:N_domain-1],sol_u_x[iter_i,0:N_domain-1]) # initialization
+        
         for iter_step in range(GN_iteration):
-            temp = jnp.linalg.solve(Hessian_GN(sol,rhs_f,bdy_g,L,dt,sol), grad_J(sol,rhs_f,bdy_g,L,dt))
+            temp = jnp.linalg.solve(Hessian_GN(sol,rhs_f,L,dt,sol), grad_J(sol,rhs_f,L,dt))
             sol = sol - step_size*temp
             total_mins = (time() - time_begin) / 60
             logging.info(f'[Timer] GP iteration {iter_step+1}/{GN_step}, finished in {total_mins:.2f} minutes')
             
-        sol_u[iter_i+1,:] = sol[:num_pts]
-        sol_u_x[iter_i+1,:] = sol[num_pts:]
-        sol_u_xx[iter_i+1,:] = (2/dt*sol[:num_pts]+sol[:num_pts]*sol[num_pts:]-rhs_f)/nu
+        sol_u[iter_i+1,:] = onp.append(sol[:num_pts-1],sol[0])
+        sol_u_x[iter_i+1,:] = onp.append(sol[num_pts-1:],sol[num_pts-1])
+        sol_u_xx[iter_i+1,:] = (2/dt*sol_u[iter_i+1,:]+5*sol_u[iter_i+1,:]**3-5*sol_u[iter_i+1,:]-rhs_f)/nu
         
         t = (iter_i+1)*dt
         logging.info(f'[Time marching] at iteration {iter_i+1}/{Nt-1}, solving eqn at time t = {t:.2f}') 
@@ -164,7 +159,7 @@ def time_steping_solve(X_domain, X_boundary, dt,T,num_pts, step_size = 1, nugget
 
 # log the results
 def logger(args, level = 'INFO'):
-    log_root = args.logroot + 'Burgers'
+    log_root = args.logroot + 'Allen_Cahn'
     log_name = 'kernel' + str(args.kernel)
     logdir = os.path.join(log_root, log_name)
     os.makedirs(logdir, exist_ok=True)
@@ -185,24 +180,18 @@ def set_random_seeds(args):
     
 # boundary condition
 def u(x1, x2):
-    return -jnp.sin(jnp.pi*x2)*(x1==0) + 0*(x2==0)
+    return (x2**2)*jnp.cos(jnp.pi*x2)
 
 def u_x(x1, x2):
-    return -jnp.pi*jnp.cos(jnp.pi*x2)*(x1==0) + 0*(x2==0)
+    return grad(u,1)(x1,x2)
 
 def u_xx(x1, x2):
-    return (jnp.pi**2)*jnp.sin(jnp.pi*x2)*(x1==0) + 0*(x2==0)
+    return grad(u_x,1)(x1,x2)
 
 # right hand side
 def f(x1, x2):
     return 0
 
-# get the truth
-def u_true(x1, x2):
-    temp = x2-jnp.sqrt(4*nu*x1)*Gauss_pts
-    val1 = weights * jnp.sin(jnp.pi*temp) * jnp.exp(-jnp.cos(jnp.pi*temp)/(2*jnp.pi*nu))
-    val2 = weights * jnp.exp(-jnp.cos(jnp.pi*temp)/(2*jnp.pi*nu))
-    return -jnp.sum(val1)/jnp.sum(val2)
 
 # plot parameters
 def set_plot():
@@ -230,7 +219,7 @@ def set_plot():
     return fmt
 
 if __name__ == '__main__':
-    ## get argument parser
+    # get argument parser
     args = get_parser()
     logger(args, level = 'INFO')
     logging.info(f'argument is {args}')
@@ -247,7 +236,7 @@ if __name__ == '__main__':
     N_domain = args.N_domain
     dt = args.dt
     T = args.T
-    X_domain, X_boundary = sample_points(N_domain, dt, T, option = 'grid')
+    X_domain = sample_points(N_domain, dt, T, option = 'grid')
     sigma = args.sigma
     nugget = args.nugget
     GN_step = args.GNsteps
@@ -255,23 +244,28 @@ if __name__ == '__main__':
     logging.info(f'GN step: {GN_step}, sigma: {sigma}, number of points: N_domain {N_domain}, kernel: {args.kernel}, nugget: {args.nugget}')
     
     # solve the equation
-    sol_domain = time_steping_solve(X_domain, X_boundary, dt, T, N_domain, step_size = 1, nugget = nugget, sigma=sigma, GN_iteration = GN_step)
+    sol_domain = time_steping_solve(X_domain, dt, T, N_domain, step_size = 1, nugget = nugget, sigma=sigma, GN_iteration = GN_step)
     
     logging.info('[Calculating errs at collocation points ...]')
     
-    # true solution:
-    # obtain the ground truth solution via the Cole-Hopf transformation
-    # we use numerical integration to get the true solution
-    [Gauss_pts, weights] = onp.polynomial.hermite.hermgauss(80)
-    y_true = vmap(u_true)(X_domain[:,:,0].flatten(),X_domain[:,:,1].flatten())
-    Nt = int(T/dt)+1
-    y_true = onp.reshape(y_true, (Nt,N_domain))
+    # # true solution:
+    # # obtain the ground truth solution via the Cole-Hopf transformation
+    # # we use numerical integration to get the true solution
+    data = scipy.io.loadmat('./Allen-Cahn/AC.mat')
+    # T = 1/0.005+1
+    # N = 2/2^(-8)
+    t = data['tt'].flatten()[:,None] # T x 1
+    x = data['x'].flatten()[:,None] # N x 1
+    Exact = onp.real(data['uu']).T # T x N
     
-    # compute errors
+    ratio = int(dt/0.005)
+    y_true = Exact[::ratio,:]
+
+    # # compute errors
     err = abs(sol_domain[:,:]-y_true) 
     err_t_1 = err[-1,:]
     L2err_t_1 = onp.sqrt(sum(err_t_1**2)/N_domain)
-    Maxerr_t_1 = max(err[-1,:])
+    Maxerr_t_1 = max(err_t_1)
     logging.info(f'[Errors] L2 err at t=1 is {L2err_t_1} and Max err is {Maxerr_t_1}')
     
     
